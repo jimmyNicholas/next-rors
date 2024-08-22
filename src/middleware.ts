b8e0a6236e1dd20aeed102d7e90fb95b6b750317
jimmyNicholas <jimmynicholas@duck.com>
2024-08-22 21:20:22 +1000

    middleware bug fixes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,40 +1,5 @@
+import { NextRequest, NextResponse } from 'next/server';
 
-/*AT THIS STAGE VERCEL DOES NOT SUPPORT CLERK AUTH WITHOUT A PRO ACCOUNT*/
-
-/*
-import { clerkMiddleware, createRouteMatcher  } from "@clerk/nextjs/server";
-
-const isProtectedRoute = createRouteMatcher(['/dashboard(.*)'])
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)'])
-
-export default clerkMiddleware((auth, request) => {
-  
-    if (!isPublicRoute(request)) {
-      auth().protect();
-    }
-
-    if (isProtectedRoute(request)) {
-      auth().protect()
-    };
-    
-    if (isProtectedRoute(request)) {
-      auth().protect((has) => {
-        
-        return (
-          has({ role: 'org:admin' }) ||
-          has({ role: 'org:member' })
-        )
-      })
-    }
-  
-});
-
-export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
-};
-*/
+export default function middleware(req: NextRequest) {
+    return NextResponse.next();
+}
